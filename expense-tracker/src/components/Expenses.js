80b1f6821e97e83db84e42111db06adedccd837a
NirmalViswajith <nikanomi06@gmail.com>
2023-07-02 17:55:05 +0530

    Added Location

--- expense-tracker/src/components/Expenses.js
+++ expense-tracker/src/components/Expenses.js
@@ -1,12 +1,18 @@
+import "./Expenses.css";
 function Expenses() {
+  const expenseDate = new Date(2021,2,28);
+  const expenseTitle = 'Car Insurance';
+  const itemPrice = '$1000';
+  const locationOfExpenditure = 'Bank Of Baroda';
   return (
     <>
       <h1>Expense Items</h1>
-      <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ul>
+      <div className="expenseItem">
+        <div>{expenseDate.toISOString()}</div>
+        <h2>{expenseTitle}</h2>
+        <div>{itemPrice}</div>
+        <div>{locationOfExpenditure}</div>
+      </div>
     </>
   );
 }
