60ff6cd14618f76a28c83d32be52474c779beb92
NirmalViswajith <nikanomi06@gmail.com>
2023-07-02 18:08:31 +0530

    Applied looping if there is a 100 items

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,44 @@
 import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      date: "March 21",
+      title: "Car Insurance",
+      price: "1000",
+      location: "Banglore"
+    },
+    {
+      date: "March 22",
+      title: "Hotel",
+      price: "870",
+      location: "Banglore"
+    },
+    {
+      date: "March 23",
+      title: "Shopping",
+      price: "10000",
+      location: "Banglore"
+    },
+    {
+      date: "March 24",
+      title: "Party",
+      price: "10000",
+      location: "Banglore"
+    }
+  ]
   return (
     <div className="App">
-      <Expenses />
+      <h1>Expense Items</h1>
+      {expenses.map((expense, index) => (
+        <Expenses
+          key={index}
+          date={expense.date}
+          title={expense.title}
+          price={expense.price}
+          location={expense.location}
+        />
+      ))}
     </div>
   );
 }
