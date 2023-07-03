1c68c98dbff776cb1cce5d54ee7ab21b450e9530
NirmalViswajith <nikanomi06@gmail.com>
2023-07-03 12:58:15 +0530

    onChange

--- expense-tracker/src/components/Form/ExpenseForm.js
+++ expense-tracker/src/components/Form/ExpenseForm.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 
 const ExpenseForm = () => {
+  const inputChange = (event) => {
+    console.log(event.target.value);
+  }
   return (
     <Form>
       <Form.Group controlId='title' >
         <Form.Label style={{ color: 'black' }}>Expense Title</Form.Label>
-        <Form.Control type='text' style={{ borderColor: 'black' }} />
+        <Form.Control type='text' style={{ borderColor: 'black' }} onChange={inputChange} />
       </Form.Group>
       <Form.Group controlId='amount'>
         <Form.Label style={{ color: 'black' }}>Expense Amount</Form.Label>
@@ -15,7 +18,7 @@ const ExpenseForm = () => {
       <Form.Group controlId='date'>
         <Form.Label style={{ color: 'black' }}>Expense date</Form.Label>
         <Form.Control type='date' style={{ borderColor: 'black' }} />
-        <button className='btn-dark my-3'>
+        <button className='btn-dark my-3' >
           Add Expenses
         </button>
       </Form.Group>
