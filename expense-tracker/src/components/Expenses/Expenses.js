cbf3b124edaabbcadaddea04e440e7bfc1508b96
NirmalViswajith <nikanomi06@gmail.com>
2023-07-03 09:04:34 +0530

    Added Delete Button

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -2,11 +2,23 @@ import "./Expenses.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
+
 function Expenses(props) {
+  const changeTitle = () => {
+    console.log('Clicked!!!')
+  }
+  const delEvent = () => {
+    const expenseItem = document.getElementById(props.id);
+    if (expenseItem) {
+      expenseItem.remove();
+    }
+  }
   return (
     <Card className="expenseItem">
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} amount={props.amount} location={props.location} />
+      <button onClick={changeTitle}>Change Title</button>
+      <button onClick={delEvent}>Delete Expense</button>
     </Card>
   );
 }
