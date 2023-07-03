a2fa226e2c8838dda340e07e0ef4f806bdf58be5
NirmalViswajith <nikanomi06@gmail.com>
2023-07-03 18:24:52 +0530

    Multiple Inputs

--- expense-tracker/src/components/Form/ExpenseForm.js
+++ expense-tracker/src/components/Form/ExpenseForm.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
 const ExpenseForm = () => {
   const inputChange = (event) => {
     console.log(event.target.value);
+  };
+
+  const [enteredTitle, updatedTitle] = useState("");
+  const newTitle = (event) => {
+    updatedTitle(event.target.value);
   }
+  const [enteredAmount, updatedAmount] = useState("");
+  const newAmount = (event) => {
+    updatedTitle(event.target.value);
+  }
+  const [enteredDate, updatedDate] = useState("");
+  const newDate = (event) => {
+    updatedDate(event.target.value);
+  }
+
   return (
-    <Form>
-      <Form.Group controlId='title' >
-        <Form.Label style={{ color: 'black' }}>Expense Title</Form.Label>
-        <Form.Control type='text' style={{ borderColor: 'black' }} onChange={inputChange} />
-      </Form.Group>
-      <Form.Group controlId='amount'>
-        <Form.Label style={{ color: 'black' }}>Expense Amount</Form.Label>
-        <Form.Control type='number' min="0.01" step="0.01" style={{ borderColor: 'black' }} />
-      </Form.Group>
-      <Form.Group controlId='date'>
-        <Form.Label style={{ color: 'black' }}>Expense date</Form.Label>
-        <Form.Control type='date' style={{ borderColor: 'black' }} />
-        <button className='btn-dark my-3' >
-          Add Expenses
-        </button>
-      </Form.Group>
-    </Form>
+    <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+      <Form className='p-2'>
+        <Form.Group controlId='title'>
+          <Form.Label style={{ color: 'black' }}>Expense Title</Form.Label>
+          <Form.Control type='text' style={{ borderColor: 'black' }} onChange={newTitle} />
+        </Form.Group>
+        <Form.Group controlId='amount'>
+          <Form.Label style={{ color: 'black' }}>Expense Amount</Form.Label>
+          <Form.Control type='number' min='0.01' step='0.01' style={{ borderColor: 'black' }} onChange={newAmount} />
+        </Form.Group>
+        <Form.Group controlId='date'>
+          <Form.Label style={{ color: 'black' }}>Expense date</Form.Label>
+          <Form.Control type='date' style={{ borderColor: 'black' }} onChange={newDate} />
+          <button className='btn-dark my-3'>Add Expenses</button>
+        </Form.Group>
+      </Form>
+    </div>
   );
-}
+};
 
 export default ExpenseForm;
