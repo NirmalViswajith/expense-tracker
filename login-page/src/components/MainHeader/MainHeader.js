c7988cebbb35041ea35df455b0eee8eea261ee2a
NirmalViswajith <nikanomi06@gmail.com>
2023-07-11 12:15:01 +0530

    Implemented useContext

--- login-page/src/components/MainHeader/MainHeader.js
+++ login-page/src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <Navigation onLogout={props.onLogout} />
     </header>
   );
 };
