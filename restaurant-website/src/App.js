1d3f242ef4040a01bf1cbdc26a0f4b31f96dfe90
NirmalViswajith <nikanomi06@gmail.com>
2023-07-15 13:00:32 +0530

    Added hard cart

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Summary from "./Components/Summary";
+import Cart from "./Components/UI/Cart";
 
 
 
@@ -35,6 +36,7 @@ function App() {
 
   return (
     <div className="App">
+      <Cart />
       <Header />
       <Summary items={foodItems} />
     </div>
---
+++ restaurant-website/src/Components/UI/Cart.js
@@ -0,0 +1,28 @@
+import classes from "./Cart.Module.css";
+import Model from "./Modal";
+import Button from "react-bootstrap/Button";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className="cart-items">
+      {[{ id: "c1", name: "sushi", quantity: "2", price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <Model>
+      {cartItems}
+      <div className="d-flex justify-content-between">
+        <h4>Total Amount</h4>
+        <h4>35.62</h4>
+      </div>
+      <div className="d-flex justify-content-end">
+        <Button variant="dark" className="mr-2">Close</Button>
+        <Button variant="primary">Order</Button>
+      </div>
+    </Model>
+  );
+};
+
+export default Cart;
