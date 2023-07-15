ab76cf69422150175dc5809bea4fd9f1628b8945
NirmalViswajith <nikanomi06@gmail.com>
2023-07-15 12:00:57 +0530

    Created Form

--- restaurant-website/src/Components/FoodList.js
+++ restaurant-website/src/Components/FoodList.js
@@ -1,12 +1,28 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 const FoodList = (props) => {
   return (
-    <li>
-      <h4 className="text-xl">{props.name}</h4>
-      <p className="text-muted">{props.description}</p>
-      <h4 className="text-xl">{props.amount}</h4>
-      <hr />
-    </li>
+    <div>
+      <li className="flex justify-between">
+        <div>
+          <h4 className="text-xl">{props.name}</h4>
+          <p className="text-muted">{props.description}</p>
+          <h4 className="text-xl">{props.amount}</h4>
+        </div>
+        <div className="flex flex-col">
+          <Form>
+            <Form.Group className="flex">
+              <Form.Label>Quantity:</Form.Label>
+              <Form.Control type="number" style={{ width: "125px" }} className="ml-2" />
+            </Form.Group>
+          </Form>
+          <Button className="btn-danger rounded-pill mt-2 w-24">+Add</Button>
+        </div>
+      </li>
+      <hr className="w-full mt-4" />
+    </div>
   );
-}
+};
 
 export default FoodList;
