17638f312b4e7a7ff3f0511db31c61ff64a973b6
NirmalViswajith <nikanomi06@gmail.com>
2023-07-19 19:38:06 +0530

    Used Router

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
-import Header from './Components/Header';
-import Products from './Components/HomePage/Products';
-import Cart from './Components/UI/Cart';
-import CartProvider from './Store/CartProvider';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
+import NavBar from "./Components/HomePage/NavBar";
+import Header from "./Components/HomePage/Header";
+import Products from "./Components/HomePage/Products";
+import Cart from "./Components/UI/Cart";
+import CartProvider from "./Store/CartProvider";
+import About from "./Components/AboutPage/About";
 
 function App() {
   const [cartShow, setCartShow] = useState(false);
@@ -20,44 +22,51 @@ function App() {
 
   const productsArr = [
     {
-      id:'e1',
+      id: "e1",
       title: "Colors",
       price: 100,
       imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
     },
     {
-      id:'e2',
+      id: "e2",
       title: "Black and white Colors",
       price: 50,
       imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
     },
     {
-      id:'e3',
+      id: "e3",
       title: "Yellow and Black Colors",
       price: 70,
       imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
     },
     {
-      id:'e4',
+      id: "e4",
       title: "Blue Color",
       price: 100,
       imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
     },
   ];
-  
+
   return (
     <CartProvider>
-      {cartShow && <Cart products={productsArr} onClose={hideCartHandler} onRemoveCart={() => setCartItemCount((prevCount) => prevCount - 1)}/>}
+      {cartShow && (
+        <Cart
+          products={productsArr}
+          onClose={hideCartHandler}
+          onRemoveCart={() => setCartItemCount((prevCount) => prevCount - 1)}
+        />
+      )}
       <NavBar onOpen={showCartHandler} cartItemCount={cartItemCount} />
       <Header />
-      <Products
-        productsArr={productsArr}
-        onAddToCart={() => setCartItemCount((prevCount) => prevCount + 1)}
-      />
+      <Routes>
+      <Route path="/home" element={<Products productsArr={productsArr} onAddToCart={() => setCartItemCount((prevCount) => prevCount + 1)} />} />
+      <Route path="/about" element={<About />} />
+      
+      </Routes>
     </CartProvider>
   );
 }
--- e-commerce/src/Components/HomePage/Header.js
+++ e-commerce/src/Components/HomePage/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="bg-stone p-5 text-center">
+    <div className="bg-stone p-5 text-center" style={{marginTop:'90px'}}>
       <h1>The Generics</h1>
     </div>
   );
