c4d16122b31938b64df9eeaefbb689ce6e6c74d6
NirmalViswajith <nikanomi06@gmail.com>
2023-07-21 09:33:09 +0530

    VAlue Logged

--- demo/src/components/Form.js
+++ demo/src/components/Form.js
@@ -1,26 +1,70 @@
-import {Form , Button, Container} from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Container } from "react-bootstrap";
 const MyForm = () => {
-  return(
-    <Container className='border border-dark rounded bg-light my-4 p-2'>
-      <Form>
+  const [title, setTitle] = useState("");
+  const titleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const [textcrawl, setTextCrawl] = useState("");
+  const textAreaHandler = (event) => {
+    setTextCrawl(event.target.value);
+  };
+  const [date, setdate] = useState("");
+  const dateHandler = (event) => {
+    setdate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const movies = {
+      episode_id: Math.random().toString(),
+      title: title,
+      release_date: date,
+      opening_crawl: textcrawl,
+    }
+    console.log(movies);
+    setTitle('');
+    setTextCrawl('');
+    setdate('');
+  };
+
+  return (
+    <Container className="border border-dark rounded bg-light my-4 p-2">
+      <Form onSubmit={submitHandler}>
         <Form.Group>
           <Form.Label>Title:</Form.Label>
-          <Form.Control type='text'></Form.Control>
+          <Form.Control
+            type="text"
+            value={title}
+            onChange={titleHandler}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
           <Form.Label>Opening Text:</Form.Label>
-          <Form.Control type='text' as='textarea' row={8}></Form.Control>
+          <Form.Control
+            type="text"
+            as="textarea"
+            row={8}
+            value={textcrawl}
+            onChange={textAreaHandler}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
           <Form.Label>Release Date:</Form.Label>
-          <Form.Control type='date'></Form.Control>
+          <Form.Control
+            type="date"
+            value={date}
+            onChange={dateHandler}
+          ></Form.Control>
         </Form.Group>
-        <div className='d-flex justify-content-center'>
-        <Button variant='outline-dark' className='my-4'>Add Movies</Button>
+        <div className="d-flex justify-content-center">
+          <Button type="submit" variant="outline-dark" className="my-4">
+            Add Movies
+          </Button>
         </div>
       </Form>
     </Container>
   );
-}
+};
 
 export default MyForm;
