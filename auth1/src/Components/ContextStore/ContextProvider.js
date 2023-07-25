45d2d0cdb32de78cb790b000fe7c94dbd032143a
NirmalViswajith <nikanomi06@gmail.com>
2023-07-25 20:37:43 +0530

    auto logout functionality

--- auth1/src/Components/ContextStore/ContextProvider.js
+++ auth1/src/Components/ContextStore/ContextProvider.js
@@ -1,39 +1,53 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = React.createContext({
-  token: '',
+  token: "",
   isLoggedIn: false,
   login: (token) => {},
-  logout: () => {}
-})
+  logout: () => {},
+});
 
 export const AuthProvider = (props) => {
-  const initialState = localStorage.getItem('token');
+  const initialState = localStorage.getItem("token");
   const navigate = useNavigate();
   const [token, setToken] = useState(initialState);
   const userisLoggedIn = !!token;
+
   const loginHandler = (token) => {
     setToken(token);
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
   };
+
   const logoutHandler = () => {
     setToken(null);
-    localStorage.removeItem('token', token);
-    navigate('/auth');
+    localStorage.removeItem("token");
+    navigate("/auth");
   };
+
+  useEffect(() => {
+    const tokenRemovalTimeout = setTimeout(() => {
+      setToken(null);
+      localStorage.removeItem("token");
+      navigate("/auth");
+    }, 300000);
+
+    // Clean up the timeout to avoid memory leaks
+    return () => clearTimeout(tokenRemovalTimeout);
+  }, [token, navigate]);
 
   const auth = {
     token: token,
     isLoggedIn: userisLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
-  }
-  return(
+  };
+
+  return (
     <AuthContext.Provider value={auth}>
       {props.children}
     </AuthContext.Provider>
   );
-}
+};
 
 export default AuthContext;
