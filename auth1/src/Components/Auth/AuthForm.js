495a1f3a7d80cd567d5a37fe36a302a7d6c11150
NirmalViswajith <nikanomi06@gmail.com>
2023-07-25 11:17:59 +0530

    added auth

--- auth1/src/Components/Auth/AuthForm.js
+++ auth1/src/Components/Auth/AuthForm.js
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import classes from './AuthForm.module.css';
 import { Button } from 'react-bootstrap';
 
 const AuthForm = () => {
+  const enteredEmail = useRef();
+  const enteredPassword = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -12,14 +15,43 @@ const AuthForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-
-    // Simulate loading for 2 seconds, replace this with your actual authentication request
+    const details = {
+      email: enteredEmail.current.value,
+      password: enteredPassword.current.value,
+      returnSecureToken: true,
+    };
+  
     setIsLoading(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      alert('Email Exists');
-    }, 2000);
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD4AGudc26asu08SmQDiibfZk5QRfHJMVo';
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD4AGudc26asu08SmQDiibfZk5QRfHJMVo';
+    }
+  
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(details),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Authentication failed.'); 
+        }
+      })
+      .then((data) => {
+        console.log('idToken (JWT):', data.idToken);
+      })
+      .catch((error) => {
+        alert(error.message); 
+      });
   };
+  
 
   return (
     <section className={classes.auth}>
@@ -28,11 +60,11 @@ const AuthForm = () => {
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={enteredEmail}/>
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={enteredPassword}/>
         </div>
         <div className={classes.actions}>
           {isLoading ? (
