1983706f8d5657ec1730259c5bf3e54dba411569
NirmalViswajith <nikanomi06@gmail.com>
2023-08-08 10:00:30 +0530

    mailbox-client signup page

---
+++ mailbox-client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUp from './Components/Auth/SignUp';
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
---
+++ mailbox-client/src/Components/Auth/SignUp.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
+const SignUp = () => {
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const details = {
+      email: mail,
+      password: password,
+    }
+    console.log(details);
+    await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWOgewXwJ6MF_blTFfNdTdCj3c5sehREw', {
+      method: 'POST', 
+      body: JSON.stringify({
+        ...details,
+        returnSecureToken: true
+      }), 
+      headers: {
+        'Content-type' : 'application/json',
+      }
+    }).then((res) => {
+      if(res.ok) {  
+        alert('Account created')
+        return res.json()
+      } else {
+        throw new Error('Authentication failed');
+      }
+    }).then((data) => {
+      console.log(data);
+    }).catch(err => console.log(err));
+    setMail('');
+    setPassword('');
+    setConfirmPassword('');
+  }
+
+  const correctPassword = password === confirmPassword;
+  const inputFilled = mail.trim() !== '' && password !== '' && confirmPassword !== '';
+
+  return (
+    <div className='p-2'>
+      <Container className='border rounded shadow p-3 my-5' style={{ maxWidth: '650px' }}>
+        <div className='d-flex justify-content-center mb-3'>
+          <h1>Sign Up</h1>
+        </div>
+        <Form onSubmit={submitHandler} >
+          <Form.Group className='form-floating mb-3'>
+            <Form.Control type='email' id='entermail' placeholder='entermail' value={mail} onChange={(event) => setMail(event.target.value)} />
+            <Form.Label htmlFor='entermail'>Enter MailID</Form.Label>
+          </Form.Group>
+          <Form.Group className='form-floating mb-3'>
+            <Form.Control type='password' id='enterpassword' placeholder='enterpassword' value={password} onChange={(event) => setPassword(event.target.value)} />
+            <Form.Label htmlFor='enterpassword'>Enter Password</Form.Label>
+          </Form.Group>
+          <Form.Group className='form-floating mb-2'>
+            <Form.Control type='password' id='confirmpassword' placeholder='confirmpassword' value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+            <Form.Label htmlFor='confirmpassword'>Confirm Password</Form.Label>
+          </Form.Group>
+          <div className='d-flex justify-content-center mt-3'>
+            <Button variant='success' type='submit' className='rounded' disabled={!inputFilled || !correctPassword}>Sign Up</Button>
+          </div>
+        </Form>
+      </Container>
+    </div>
+  );
+}
+
+export default SignUp;
